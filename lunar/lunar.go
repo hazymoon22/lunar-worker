e98d49bf60d52420327d18eea7c8ec83879b5e65
huyb-cl <huy.bui@codelink.io>
2026-02-25 21:40:31 +0700

    feat: :sparkles: Implement helper logic for handling, converting Lunar dates

---
+++ lunar/lunar.go
@@ -0,0 +1,45 @@
+package lunar
+
+import (
+	"time"
+
+	"github.com/6tail/lunar-go/calendar"
+)
+
+func GetLunarCurrentYear(lunar calendar.Lunar) *calendar.Lunar {
+	now := time.Now()
+	year := now.Year()
+	month := lunar.GetMonth()
+	day := lunar.GetDay()
+
+	return calendar.NewLunarFromYmd(year, month, day)
+}
+
+func GetLunarNextYear(lunar calendar.Lunar) *calendar.Lunar {
+	now := time.Now()
+	year := now.Year()
+	targetYear := year + 1
+	month := lunar.GetMonth()
+	day := lunar.GetDay()
+
+	return calendar.NewLunarFromYmd(targetYear, month, day)
+}
+
+func GetLunarCurrentMonth(lunar calendar.Lunar) *calendar.Lunar {
+	now := time.Now()
+	year := now.Year()
+	month := now.Month()
+	day := lunar.GetDay()
+
+	return calendar.NewLunarFromYmd(year, int(month), day)
+}
+
+func GetLunarNextMonth(lunar calendar.Lunar) *calendar.Lunar {
+	now := time.Now()
+	year := now.Year()
+	month := int(now.Month())
+	targetMonth := month + 1
+	day := lunar.GetDay()
+
+	return calendar.NewLunarFromYmd(year, targetMonth, day)
+}
